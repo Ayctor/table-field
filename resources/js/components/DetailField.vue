1815778a23e7cbbc77e5e82dbf1c41f027b57392
Axel Charpentier <axelc@ayctor.com>
2020-05-26 11:25:26 +0200

    fix responsive table

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,22 +1,26 @@
 <template>
     <panel-item :field="field">
         <template slot="value">
-            <table class="w-full border border-collapse">
-                <thead>
-                    <tr>
-                        <th class="p-2 border text-left" v-for="(column, index) in data[0]" :key="index">
-                            {{ column }}
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(row, index) in data" :key="index">
-                        <td class="p-2 border" v-for="(column, subindex) in row" :key="subindex" v-if="index !== 0">
-                            {{ column }}
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="w-full overflow-hidden">
+                <div class="overflow-x-auto">
+                    <table class="w-full border border-collapse">
+                        <thead>
+                            <tr>
+                                <th class="p-2 border text-left" v-for="(column, index) in data[0]" :key="index">
+                                    {{ column }}
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="(row, index) in data" :key="index">
+                                <td class="p-2 border" v-for="(column, subindex) in row" :key="subindex" v-if="index !== 0">
+                                    {{ column }}
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </template>
     </panel-item>
 </template>
